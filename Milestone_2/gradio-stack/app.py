93e80ccbcf9825e04c4c0e9b3fcea6024d21e350
LukesScammell <luke-scammell@live.nmit.ac.nz>
2026-09-23 16:26:20 +1200

    Set up Gradio framework prototype

---
+++ Milestone_2/gradio-stack/app.py
@@ -0,0 +1,18 @@
+import gradio as gr
+
+
+# Creates the basic Gradio interface for the Pixel Pals prototype.
+with gr.Blocks(title="Pixel Pals") as app:
+    gr.Markdown("# Pixel Pals")
+    gr.Markdown("Gradio framework prototype for WEB701.")
+
+    gr.Markdown("## Account System")
+    gr.Markdown("Registration and login functionality will be added here.")
+
+    gr.Markdown("## Token Transaction")
+    gr.Markdown("Token transaction functionality will be added here.")
+
+
+# Starts the Gradio application.
+if __name__ == "__main__":
+    app.launch()
